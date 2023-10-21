48263479dd95a0e78983dfc3bb4be77272fe7174
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-21 09:23:43 +0300

    Created the HomeScreen component file

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+type Props = {}
+
+const HomeScreen = (props: Props) => {
+	return <View>HomeScreen</View>
+}
+
+export default HomeScreen
