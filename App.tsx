13e71b5ebe735f8899cb7c5c35bc809ba766cc29
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-21 09:45:34 +0300

    Simplified the App component

--- App.tsx
+++ App.tsx
@@ -1,22 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import HomeScreen from './screens/HomeScreen'
 import Navigation from './navigation'
 
 export default function App() {
 	return (
-		<View style={styles.container}>
+		<>
 			<Navigation />
 			<StatusBar style='auto' />
-		</View>
+		</>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
-})
