43ff8ea5ed86f092234593112da9f4d359310d00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-21 09:38:12 +0300

    Added imports for the NavigationContainer and createNativeStackNavigator

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { View } from 'react-native'
 
 type Props = {}
