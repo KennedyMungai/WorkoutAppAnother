70828cf4e6e657d7298f9155e9d2834b6a47282f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-21 12:36:02 +0300

    Refactored the component

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,21 +2,25 @@ import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 import React from 'react'
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import data from '../data.json'
+import { WorkOut } from '../types/data'
 
 const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
+	const renderStuff = ({ item }: { item: WorkOut }) => {
+		return (
+			<View>
+				<Text>
+					<Text>{item.name}</Text> - <Text>{item.difficulty}</Text>
+				</Text>
+			</View>
+		)
+	}
+
 	return (
 		<View style={styles.homeScreenView}>
 			<FlatList
 				data={data}
 				keyExtractor={(itemData) => itemData.slug}
-				renderItem={({ item }) => (
-					<View>
-						<Text>
-							<Text>{item.name}</Text> -{' '}
-							<Text>{item.difficulty}</Text>
-						</Text>
-					</View>
-				)}
+				renderItem={renderStuff}
 			/>
 		</View>
 	)
