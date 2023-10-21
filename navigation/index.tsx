073374e1de9c5d877c09e7f8e04292b584548c46
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-21 11:35:17 +0300

    Using the vector icons for the tabBar

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from '../screens/HomeScreen'
 import PlannerScreen from '../screens/PlannerScreen'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { FontAwesome5 } from '@expo/vector-icons'
 
 type Props = {}
 
@@ -38,12 +39,26 @@ const BottomTabNavigator = () => {
 			<BottomTab.Screen
 				name='Home'
 				component={HomeScreen}
-				// options={{ unmountOnBlur: true }}
+				options={{
+					unmountOnBlur: true,
+					tabBarIcon: () => (
+						<FontAwesome5 name='home' size={24} color='black' />
+					)
+				}}
 			/>
 			<BottomTab.Screen
 				name='Planner'
 				component={PlannerScreen}
-				// options={{ unmountOnBlur: true }}
+				options={{
+					unmountOnBlur: true,
+					tabBarIcon: () => (
+						<FontAwesome5
+							name='calendar-alt'
+							size={24}
+							color='black'
+						/>
+					)
+				}}
 			/>
 		</BottomTab.Navigator>
 	)
