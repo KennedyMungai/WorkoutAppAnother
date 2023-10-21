57244fc4f704023505ee88b87b8d91f27eed9d5d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-21 12:40:36 +0300

    Created the WorkoutItem component

---
+++ components/WorkoutItem.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+type Props = {}
+
+const WorkoutItem = (props: Props) => {
+	return (
+		<View>
+			<Text>WorkoutItem</Text>
+		</View>
+	)
+}
+
+export default WorkoutItem
