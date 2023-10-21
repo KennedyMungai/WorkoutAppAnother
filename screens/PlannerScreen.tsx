56a1c6bc36640ff77f9d886391b14fe149d767af
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-21 09:24:43 +0300

    Added the PlannerScreen component

---
+++ screens/PlannerScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+type Props = {}
+
+const PlannerScreen = (props: Props) => {
+	return <View>PlannerScreen</View>
+}
+
+export default PlannerScreen
