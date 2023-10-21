923647b0a15ccf6b673f45eff6edef571c4a2049
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-21 09:57:00 +0300

    Styled the planner screen

--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Button, Text, View, Alert, StyleSheet } from 'react-native'
 
 type Props = {}
 
 const PlannerScreen = (props: Props) => {
 	return (
-		<View>
+		<View style={styles.plannerScreenView}>
 			<Text>PlannerScreen</Text>
+			<Button
+				title='Go to Home'
+				onPress={() => Alert.alert('Go to Home')}
+			/>
 		</View>
 	)
 }
 
 export default PlannerScreen
+
+const styles = StyleSheet.create({
+	plannerScreenView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 10
+	}
+})
