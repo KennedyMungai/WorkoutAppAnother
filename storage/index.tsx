e3896d6fc3cfdc057351cc315847c9bfe9041ead
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 15:06:00 +0300

    Added some logic to the storeData arrow function

--- storage/index.tsx
+++ storage/index.tsx
@@ -2,6 +2,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 type Props = {}
 
-export const storeData = async () => {}
+export const storeData = async (key: string, value: any) => {
+	try {
+		const stringValue = JSON.stringify(value)
+		await AsyncStorage.setItem(key, stringValue)
+	} catch (error: any) {
+		console.error(error.message)
+	}
+}
 
 export const getData = async () => {}
