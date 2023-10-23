92ad2a85b70046d77f0b2ca09c557c7e541fcb3a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 12:37:55 +0300

    Copied over the markup from the HomeScreen

--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -1,13 +1,41 @@
-import { View, Text } from 'react-native'
+import { NativeStackHeaderProps } from '@react-navigation/native-stack'
+import React from 'react'
+import { Alert, FlatList, Pressable, StyleSheet, View } from 'react-native'
+import WorkoutItem from '../components/WorkoutItem'
+import data from '../data.json'
+import { WorkOut } from '../types/data'
 
-type Props = {}
-
-const WorkoutDetailScreen = (props: Props) => {
+const WorkoutDetailScreen = ({ navigation }: NativeStackHeaderProps) => {
 	return (
-		<View>
-			<Text>Workout Detail Screen</Text>
+		<View style={styles.homeScreenView}>
+			<FlatList
+				data={data as WorkOut[]}
+				keyExtractor={(itemData) => itemData.slug}
+				renderItem={({ item }) => {
+					return (
+						<Pressable
+							onPress={() =>
+								Alert.alert(`I am pressed - ${item.name}`)
+							}
+						>
+							<WorkoutItem item={item} />
+						</Pressable>
+					)
+				}}
+			/>
 		</View>
 	)
 }
 
 export default WorkoutDetailScreen
+
+const styles = StyleSheet.create({
+	homeScreenView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 10,
+		padding: 20,
+		backgroundColor: '#5B6057'
+	}
+})
