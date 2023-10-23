4adb7e3cae8b2cef3b17655938e0d1fc2d6e1c4b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 12:35:26 +0300

    Created the workout detail screen

---
+++ screens/WorkoutDetailScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+type Props = {}
+
+const WorkoutDetailScreen = (props: Props) => {
+	return (
+		<View>
+			<Text>Workout Detail Screen</Text>
+		</View>
+	)
+}
+
+export default WorkoutDetailScreen
