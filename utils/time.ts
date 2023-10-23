0c6f6b5aa2f8e9b7315b88e1727daa274bffdb64
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 11:54:15 +0300

    Added some more formatting rules for the formatSec function

--- utils/time.ts
+++ utils/time.ts
@@ -6,5 +6,15 @@ export const formatSec = (sec: number): string => {
     const _min = Math.floor(sec / 60)
     const _sec = sec % 60
 
-    return `${_min} minutes and ${_sec} seconds`
+    let result = `${_min} minutes and ${_sec} seconds`
+
+    if (_min < 1) {
+        result = `${_sec} seconds`
+    }
+
+    if (_sec < 1) {
+        result = `${_min} minutes`
+    }
+
+    return result
 }
