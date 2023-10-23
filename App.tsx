fbb97bb192d4dc337179b42be2971d1ec755771e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 09:38:38 +0300

    Made some changes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,7 @@
 import { StatusBar } from 'expo-status-bar'
 import Navigation from './navigation'
-import useCachedResources from './hooks/useCachedResources'
 
 export default function App() {
-	const isLoaded = useCachedResources()
-
-	console.log(isLoaded)
-
 	return (
 		<>
 			<Navigation />
