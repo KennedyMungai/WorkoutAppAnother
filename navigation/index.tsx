b2f07055cc71c71c9ef52756187b320a1a3111b4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 14:13:41 +0300

    Reorganized the imports

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,9 +1,9 @@
+import { FontAwesome5 } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from '../screens/HomeScreen'
 import PlannerScreen from '../screens/PlannerScreen'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { FontAwesome5 } from '@expo/vector-icons'
 
 type Props = {}
 
