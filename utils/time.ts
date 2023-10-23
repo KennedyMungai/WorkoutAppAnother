861382d84cf0ee9f91d36fa3b1df6a6d04801399
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 11:43:44 +0300

    Added the formatSec function

--- utils/time.ts
+++ utils/time.ts
@@ -1,3 +1,10 @@
-export const secToMin = (sec: number) => {
-    return Math.floor(sec / 60)
+export const secToMin = (sec: number): string => {
+    return (sec / 60).toFixed(1)
+}
+
+export const formatSec = (sec: number): string => {
+    const _min = Math.floor(sec / 60)
+    const _sec = sec % 60
+
+    return `${_min} minutes and ${_sec} seconds`
 }
